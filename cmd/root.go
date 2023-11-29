a37d6e038bac48f7bed70c5fdeca916c225844da
xm1k3 <73166077+xm1k3@users.noreply.github.com>
2023-11-29 09:55:15 +0100

    feat: test

--- cmd/root.go
+++ cmd/root.go
@@ -4,10 +4,18 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"log"
 	"os"
+	"time"
 
 	"github.com/spf13/cobra"
+	"github.com/warthog618/gpiod"
 )
+
+type Song struct {
+	Frequency float64
+	Duration  time.Duration
+}
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -16,7 +24,47 @@ var rootCmd = &cobra.Command{
 	Long:  `Golang test with gpiod`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	Run: func(cmd *cobra.Command, args []string) {},
+	Run: func(cmd *cobra.Command, args []string) {
+		chipFLag, _ := cmd.Flags().GetString("chip")
+		log.Fatal(chipFLag)
+		chip, err := gpiod.NewChip(chipFLag)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer chip.Close()
+
+		song := []Song{
+			{659.25, 450 * time.Millisecond},
+			{493.88, 250 * time.Millisecond},
+			{523.25, 250 * time.Millisecond},
+			{587.33, 250 * time.Millisecond},
+			{523.25, 250 * time.Millisecond},
+			{493.88, 250 * time.Millisecond},
+			{440.00, 450 * time.Millisecond},
+			{440.00, 450 * time.Millisecond},
+			{523.25, 250 * time.Millisecond},
+			{659.25, 450 * time.Millisecond},
+			{587.33, 250 * time.Millisecond},
+			{523.25, 450 * time.Millisecond},
+			{493.88, 150 * time.Millisecond},
+			{523.25, 450 * time.Millisecond},
+			{587.33, 450 * time.Millisecond},
+			{659.25, 450 * time.Millisecond},
+			{523.25, 600 * time.Millisecond},
+			{440.00, 250 * time.Millisecond},
+			{440.00, 450 * time.Millisecond},
+		}
+
+		line, err := chip.RequestLine(21, gpiod.AsOutput(0))
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer line.Reconfigure(gpiod.AsInput)
+
+		for _, note := range song {
+			playTone(line, note.Frequency, note.Duration)
+		}
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -37,5 +85,18 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().StringP("chip", "c", "gpiochip0", "Chip name")
+}
+
+func playTone(line *gpiod.Line, freq float64, duration time.Duration) {
+	high := time.Duration(float64(time.Second) / freq / 2)
+	low := high
+
+	start := time.Now()
+	for time.Since(start) < duration {
+		line.SetValue(1)
+		time.Sleep(high)
+		line.SetValue(0)
+		time.Sleep(low)
+	}
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,12 @@ module github.com/xm1k3/gogpiod
 go 1.20
 
 require (
+	github.com/spf13/cobra v1.8.0
+	github.com/warthog618/gpiod v0.8.1
+)
+
+require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/spf13/cobra v1.8.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/sys v0.10.0 // indirect
 )
